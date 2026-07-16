a9aba0f55f41164283e14e5cd21ce67161e10435
Mehdi Refiyev <mehdirefiyev177@gmail.com>
2026-07-17 00:49:51 +0400

    46 derse baslamisam ama bitirmemisem

---
+++ src/app/dialog/edit-task-dialog/edit-task-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-task-dialog',
+  imports: [],
+  templateUrl: './edit-task-dialog.html',
+  styleUrl: './edit-task-dialog.scss',
+})
+export class EditTaskDialog {
+
+}
